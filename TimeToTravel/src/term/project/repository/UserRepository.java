83571acc286f524f00a1018905e8fe391cd2a92d
devwithpug <devwithpug@kakao.com>
2021-10-05 00:10:07 +0900

    feature: UserRepository 쿼리 추가

--- TimeToTravel/src/term/project/repository/UserRepository.java
+++ TimeToTravel/src/term/project/repository/UserRepository.java
@@ -33,7 +33,7 @@ public class UserRepository implements Repository<User, String> {
     @Override
     public User getOneById(String id) throws SQLException {
 
-        String sql = "";
+        String sql = "select * from user u where u.user_id = ?";
 
         Connection conn = DBUtil.getConn();
         PreparedStatement stmt = conn.prepareStatement(sql);
@@ -51,6 +51,45 @@ public class UserRepository implements Repository<User, String> {
             );
         }
         return null;
+    }
+
+    public User getOneByEmailAndPassword(String email, String password) throws SQLException {
+
+        String sql = "select * from user u where u.email = ? and u.password = ?";
+
+        Connection conn = DBUtil.getConn();
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setString(1, email);
+        stmt.setString(2, password);
+        ResultSet rs = stmt.executeQuery();
+
+        if (rs.next()) {
+            return new User(
+                    rs.getString(1),
+                    rs.getString(2),
+                    rs.getString(3),
+                    rs.getString(4),
+                    rs.getString(5),
+                    rs.getString(6)
+            );
+        }
+        return null;
+
+    }
+
+    public boolean isExistsByEmail(String email) throws SQLException {
+
+        String sql = "select count(*) from user u where u.email = ?";
+
+        Connection conn = DBUtil.getConn();
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setString(1, email);
+        ResultSet rs = stmt.executeQuery();
+
+        rs.next();
+        int count = rs.getInt(1);
+
+        return count != 0;
     }
 
     @Override
